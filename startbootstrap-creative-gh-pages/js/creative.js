baaee854d26e30900320626a432e326bf9b3d036
matthewK <matthew.kiernander@healthlink.net>
2017-01-19 15:31:52 +1300

    Changed css pointers, implemented video popups.

--- startbootstrap-creative-gh-pages/js/creative.js
+++ startbootstrap-creative-gh-pages/js/creative.js
@@ -26,7 +26,7 @@
         offset: {
             top: 100
         }
-    })
+    });
 
     // Initialize and Configure Scroll Reveal Animation
     window.sr = ScrollReveal();
@@ -57,7 +57,7 @@
             preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
         },
         image: {
-            tError: '<a href="%url%">The image giiiii #%curr%</a> could not be loaded.'
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
         }
     });
 
@@ -76,7 +76,17 @@
         }
     });
 
+    $(document).ready(function() {
+        $('.popup-youtube, .popup-vimeo, .popup-gmaps').magnificPopup({
+            disableOn: 700,
+            type: 'iframe',
+            mainClass: 'mfp-fade',
+            removalDelay: 160,
+            preloader: false,
 
+            fixedContentPos: false
+        });
+    });
 
 
 
