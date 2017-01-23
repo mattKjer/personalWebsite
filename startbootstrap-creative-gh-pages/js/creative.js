b9c290eddfe8f59e6a85cd4b3c446292fba1451c
nandapanda <mknander6@gmail.com>
2017-01-23 22:10:51 +1300

    Videography and Photography Assets added

--- startbootstrap-creative-gh-pages/js/creative.js
+++ startbootstrap-creative-gh-pages/js/creative.js
@@ -68,16 +68,165 @@
         //         ]
         //
         // });
-        $('#photography-box').magnificPopup({
+        $('#animals').magnificPopup({
             items: [
                 {
-                    src: 'http://i.imgur.com/I5hYems.png',
-                    title: 'Peter & Paul fortress in SPB',
+                    src: 'http://i.imgur.com/ycQi3CZ.jpg',
+                    title: 'Caute Rabbit',
                 },
                 {
-                    src: 'http://vimeo.com/123123',
-                    type: 'iframe' // this overrides default type
+                    src: 'http://i.imgur.com/OyMabkh.jpg',
+                    title: 'Train rides - Omaha',
                 },
+                {
+                    src: 'http://i.imgur.com/TMpjazz.jpg',
+                    title: 'Donkey that ate my hat - Omaha',
+                },
+                {
+                    src: 'http://i.imgur.com/SH3iUh9.jpg',
+                    title: 'Caute child feeding Goat Oranges - Omaha',
+                },
+                {
+                    src: 'http://i.imgur.com/Rhkw57B.jpg',
+                    title: 'Parrots - Keri Keri',
+                },
+                {
+                    src: 'http://i.imgur.com/neZjJwu.jpg',
+                    title: 'National Aquariam - Vancouver',
+                },
+                {
+                    src: 'http://i.imgur.com/H0nSiag.jpg',
+                    title: 'Deers - Sheepworld',
+                },
+                {
+                    src: 'http://i.imgur.com/KETYJbd.jpg',
+                    title: 'Captive wolf - Vancouver',
+                },
+            ],
+            gallery: {
+                enabled: true
+            },
+            type: 'image' // this is a default type
+        });
+
+        $('#food').magnificPopup({
+            items: [
+                {
+                    src: 'http://i.imgur.com/b4WLCHj.jpg',
+                    title: 'Christmas Log - Omaha',
+                },
+                {
+                    src: 'http://i.imgur.com/Nrs0c5a.jpg',
+                    title: 'Rice Kitchen Commercial Shoot',
+                },
+
+            ],
+            gallery: {
+                enabled: true
+            },
+            type: 'image' // this is a default type
+        });
+        $('#portraits').magnificPopup({
+            items: [
+                {
+                    src: 'http://i.imgur.com/RJyLWyW.jpg',
+                    title: 'Dora - Hawaii',
+                },
+                {
+                    src: 'http://i.imgur.com/Ehp5RHV.jpg',
+                    title: 'Me being a buffoon',
+                },
+                {
+                    src: 'http://i.imgur.com/DVoY0cz.jpg',
+                    title: 'First portrait - Man Cave',
+                },
+
+            ],
+            gallery: {
+                enabled: true
+            },
+            type: 'image' // this is a default type
+        });
+        $('#landscapes').magnificPopup({
+            items: [
+                {
+                    src: 'http://i.imgur.com/liy1BSz.jpg',
+                    title: 'Hobbiton Scarecrow - Matamata',
+                },
+                {
+                    src: 'http://i.imgur.com/jbnzC4X.jpg',
+                    title: 'Japanese Gardens - San Fran',
+                },
+                {
+                    src: 'http://i.imgur.com/zZEd2n0.jpg',
+                    title: 'Japanese Gardens - San Fran',
+                },
+                {
+                    src: 'http://i.imgur.com/UmMz2D1.jpg',
+                    title: 'Canoe - Keri Keri',
+                },
+                {
+                    src: 'http://i.imgur.com/E7aWf7k.jpg',
+                    title: 'Petals bathtub - Keri Keri',
+                },
+                {
+                    src: 'http://i.imgur.com/G7BR2FW.jpg',
+                    title: 'View from highrise apartment - Central Vancouver',
+                },
+            ],
+            gallery: {
+                enabled: true
+            },
+            type: 'image' // this is a default type
+        });
+        $('#products').magnificPopup({
+            items: [
+                {
+                    src: 'http://i.imgur.com/jxsVeKA.jpg',
+                    title: 'Space grey. So hot right now.',
+                },
+                {
+                    src: 'http://i.imgur.com/U5BgfxL.jpg',
+                    title: 'Commercial shoot for Computer Lounge',
+                },
+                {
+                    src: 'http://i.imgur.com/FEa5guO.jpg',
+                    title: 'EK Waterblock - Commerial Shoot',
+                },
+                {
+                    src: 'http://i.imgur.com/L6Qnkyy.jpg',
+                    title: 'Space grey. So hot right now.',
+                },
+            ],
+            gallery: {
+                enabled: true
+            },
+            type: 'image' // this is a default type
+        });
+        $('#misc').magnificPopup({
+            items: [
+                {
+                    src: 'http://i.imgur.com/2lAqICu.jpg',
+                    title: 'Contemporary Art Display - San Francisco',
+                },
+                {
+                    src: 'http://i.imgur.com/IrgUcZL.jpg',
+                    title: 'Contemporary Art Display - Seattle',
+                },
+                {
+                    src: 'http://i.imgur.com/RSKE38Z.jpg',
+                    title: 'Museum display - San Francisco',
+                },
+                {
+                    src: 'http://i.imgur.com/ADyuaFU.jpg',
+                    title: 'Ferrari rims - Auckland',
+                },
+                {
+                    src: 'http://i.imgur.com/QrD3fat.jpg',
+                    title: 'BMW drivers track day - South Auckland',
+                },
+
+
             ],
             gallery: {
                 enabled: true
